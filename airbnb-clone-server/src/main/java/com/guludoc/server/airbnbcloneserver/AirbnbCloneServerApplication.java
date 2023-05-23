a75b9f2f27ce9b25fcfd4379af06032c0254e4f9
Gang Wen <gang.wen@gmail.com>
2023-05-23 18:05:08 +0200

    Initial commit from start.spring.io

---
+++ airbnb-clone-server/src/main/java/com/guludoc/server/airbnbcloneserver/AirbnbCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.guludoc.server.airbnbcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneServerApplication.class, args);
+	}
+
+}
---
+++ airbnb-clone-server/src/test/java/com/guludoc/server/airbnbcloneserver/AirbnbCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guludoc.server.airbnbcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
